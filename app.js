44364f260e221d993bc9fb234a0e77a6305f9b38
Franc Cosp <franccospmeyaeso@gmail.com>
2021-04-22 17:38:18 +0000

    Franc's turn over

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // we declare a new global variable containing an array that represents the ballons map
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
+let ballonsMap = ['green', 'yellow', 'black', 'brown', 'white', 'green', 'blue', 'red', 'pink', 'purple', 
+'green', 'yellow', 'black', 'brown', 'white', 'green', 'blue', 'red', 'pink', 'purple'];
 
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
@@ -9,16 +10,21 @@ const popBalloon = (position) => {
 }
 
 const render = () => {
-    
+
     // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
+        const filterColor = ballonsMap.filter(())
+    // let changeColor = 
+    //     if(color === 'yellow'){
+    //         document.getElementsByClassName("balloon")
+    //     }
         return `<div class="balloon active"></div>`; // <--- render each balloon
     });
 
     document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
 
-    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+    if (activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
 }
 
 // this makes the "render" function trigger when the website starts existing
