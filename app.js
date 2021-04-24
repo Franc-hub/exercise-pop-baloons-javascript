20b948bbf36c503b426684704b12e256c8f911a4
Diana Ware <dianasofia.ware@gmail.com>
2021-04-24 09:48:02 +0000

    adding colors to balloons

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 // we declare a new global variable containing an array that represents the ballons map
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green', 'yellow', 'black', 'brown', 'white', 'green', 'blue', 'red', 'pink', 'purple', 
-'green', 'yellow', 'black', 'brown', 'white', 'green', 'blue', 'red', 'pink', 'purple'];
+let ballonsMap = ['green', 'yellow', 'black', 'green', 'yellow', 'black', 'green', 'yellow', 'black','green', 'yellow', 'black','green', 'yellow', 'black','green', 'yellow', 'black','green', 'yellow'];
 
 
 // poping a balloon is basically turning his color to null (no color)
-const popBalloon = (position) => {
+//const popBalloon = (position) => {
     // set the color to null on the balloon position
 
-    render();
-}
+  //  render();
+//}
+
 
 const render = () => {
 
     // convert ballons map of colors into real html balloons
-    const ballons = ballonsMap.map((color, position) => {
-            if(color === "yellow")
-            ballonsMap.find()
-      getElementbyClassName (balloon.style.background = ballonsMap [Math.floor(Math.random() * 2)]);
-
-        return `<div class="balloon active"></div>`; // <--- render each balloon
-    };
+    const ballons = ballonsMap.map((balloon) => {
+        if(balloon === "yellow") {
+              return `<div class="balloon active" style="background: yellow;"></div>`
+        } 
+        else if(balloon === "green") {
+              return `<div class="balloon active" style="background: green;"></div>`
+        } 
+        else if(balloon === "black") {
+              return `<div class="balloon active" style="background: black;"></div>`
+        } 
+      
+    });
+    
+    
 
 
     document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
 
-    if (activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+   // if (activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
 }
 
 // this makes the "render" function trigger when the website starts existing
